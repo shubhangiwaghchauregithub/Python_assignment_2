b34cbeeab7a24efdddc0953e2c24520fd50c493a
shubhangiwaghchauregithub <shubhangiwaghchaure001@gmail.com>
2025-10-20 11:25:52 +0530

    assignmnet 2

---
+++ task_1.py
@@ -0,0 +1,13 @@
+#Task 1: Check if a Number is Even or Odd
+
+num=int(input("Enter a number: "))
+if num%2==0:
+    print (f"{num} is even number")
+else:
+   print(f"{num} is odd number")
+   
+
+    
+
+
+    
